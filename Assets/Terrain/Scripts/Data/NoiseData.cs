20285897303a21016e0dd14031d4c7335d900e4b
HyoJinNam <sb04152@gmail.com>
2020-10-16 12:44:33 +0900

    Unity Terrain Height Map with Noise

---
+++ Assets/Terrain/Scripts/Data/NoiseData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class NoiseData : UpdatableData
+{
+    public Noise.NormalizeMode normalizeMode;
+
+    public float noiseScale = 40;
+
+    public int octaves = 4;
+    [Range(0, 1)]
+    public float persistance = 0.5f;
+    public float lacunarity = 2.0f;
+
+    public int seed;
+    public Vector2 offset;
+    
+    protected override void OnValidate()
+    {
+        if (lacunarity < 1) { lacunarity = 1; }
+        if (octaves < 0) { octaves = 0; }
+        base.OnValidate();
+    }
+}
---
+++ Assets/Terrain/Scripts/Data/UpdatableData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatableData : ScriptableObject
+{
+    public event System.Action OnvaluesUpdated;
+    public bool autoUpdate;
+
+    protected virtual void OnValidate() {
+        if (autoUpdate) {
+            UnityEditor.EditorApplication.update += NotifyOfUpdatedValues;
+        }
+    }
+
+    public void NotifyOfUpdatedValues()
+    {
+        UnityEditor.EditorApplication.update -= NotifyOfUpdatedValues;
+        if (OnvaluesUpdated != null) {
+            OnvaluesUpdated();
+        }
+    }
+}
