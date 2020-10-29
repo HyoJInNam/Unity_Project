1ebac3c5b4707c5b4e1ab50872a97ee2e9cadd04
HyoJinNam <sb04152@gmail.com>
2020-10-29 18:18:28 +0900

    character unity-chan import cinemachine player follow camera(TPS) create player animator(base movement): idle, walk, run move player

---
+++ Assets/Character/_Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 velocity;      
+    Vector3 LookPoint;     
+    Rigidbody myRigidbody;
+    Animator animator;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+    
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    
+    public void LookAt(Vector3 RayPoint)
+    {
+        LookPoint = RayPoint;
+        LookPoint.y = transform.position.y;
+        transform.LookAt(LookPoint);
+    }
+    
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void SetAnimatorParameters(float speed, Vector3 veloctity)
+    {
+        animator.SetFloat("speed", speed);
+        animator.SetFloat("velocityX", veloctity.x);
+        animator.SetFloat("velocityZ", veloctity.y);
+    }
+}
