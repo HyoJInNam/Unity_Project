d961642ab9fe3e6e80532e8d32641a086882d97d
HyoJinNam <sb04152@gmail.com>
2020-10-31 22:11:34 +0900

    enviroment grass shader The shader will take an input mesh, and from each vertex on the mesh generate a blade of grass using a geometry shader. The blades will have randomized dimensions and rotationsm, and be affected by wind. To control the density of the grass, tessellation will be used to subdivide the input mesh. https://roystan.net/articles/grass-shader.html
    Added grass matarial to MapPreview and TerrainGenerate
    Received the location that should be generated in the TextureData to create the grass.
    
    import skybox Assets

---
+++ Assets/Environment/Terrain/Scripts/Editor/MapPreviewEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapPreview))]
+public class MapPreviewEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapPreview mapGen = (MapPreview) target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.DrawMapInEditor();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.DrawMapInEditor();
+        }
+    }
+}
