cb003c2c1592d98f9d837517e5e25c1a84a0d3ed
HyoJinNam <sb04152@gmail.com>
2020-11-02 17:38:13 +0900

    Environment stylized water shader Ingame pause > option UI BGM

--- Assets/Environment/Terrain/Scripts/HideOnPlay.cs
+++ Assets/Environment/Terrain/Scripts/HideOnPlay.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class HideOnPlay : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
---
+++ Assets/OptionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionManager : MonoBehaviour
+{
+    public GameObject pause;
+    public GameObject canvas;
+
+    public void Pause(GameObject canvas)
+    {
+        canvas.SetActive((canvas.activeSelf) ? false : true);
+        pause.SetActive((canvas.activeSelf) ? false : true);
+    }
+
+    public void Back(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void Retry(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Continue(GameObject pause)
+    {
+        pause.SetActive((pause.activeSelf) ? false : true);
+        canvas.SetActive((pause.activeSelf) ? false : true);
+    }
+    
+}
